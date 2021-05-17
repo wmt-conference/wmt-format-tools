15b660333540f05ce68e77c91e1ba24478543ed6
Barry Haddow <barry.haddow@gmail.com>
2021-05-17 21:52:32 +0100

    stubs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = "wmtformat",
+  version = "0.1",
+  packages=['wmtformat',],
+  install_requires = [
+  ],
+  license = 'Apache License 2.0',
+  )
+
