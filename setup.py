f799955453e4ecc0a06a37166bb58f12e0ec85c0
Tom Kocmi <tomkocmi@microsoft.com>
2024-07-23 16:56:14 +0200

    increased version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = "wmtformat",
-  version = "0.3",
+  version = "0.4",
   packages=['wmtformat',],
   license = 'Apache License 2.0',
   entry_points={
